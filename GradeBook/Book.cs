f4754ac2c328d26a575a50a4cd19259852467282
Tim Aker <taaker@gmail.com>
2021-05-19 10:35:08 -0500

    Push new changes

---
+++ GradeBook/Book.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Book
+    {
+
+        private List<double> grades;
+        private string name;
+
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = 100.1;
+
+            foreach (var number in grades)
+            {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+
+                result += number;
+            }
+
+            result /= grades.Count;
+
+            Console.WriteLine($"The average grade is {result:N1}");
+            Console.WriteLine($"The high grade is {highGrade}");
+            Console.WriteLine($"The low grade is {lowGrade}");
+        }
+    }
+}
