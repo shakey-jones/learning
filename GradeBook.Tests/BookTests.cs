e8b01b69e55499b662e61762ea5a7266f3c8df01
Tim Aker <taaker@gmail.com>
2021-05-21 14:41:30 -0500

    Tests

---
+++ GradeBook.Tests/BookTests.cs
@@ -0,0 +1,45 @@
+using System;
+using Xunit;
+
+namespace GradeBook.tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void BookCalculatesAnAverageGrade()
+        {
+            // arrange
+            var book = new Book("");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.3);
+
+            // act
+            var result = book.GetStatistics();
+
+            // assert
+            Assert.Equal(85.6, result.Average, 1);
+            Assert.Equal(90.5, result.High, 1);
+            Assert.Equal(77.3, result.Low, 1);
+
+
+        }
+
+        [Fact]
+        public void BookAddsValidValues()
+        {
+            // arrange
+            var book = new Book("Value Test");
+            book.AddGrade(105);
+
+            // act
+            var result = book.grades[0];
+
+            // assert
+            Assert.Equal(105, result, 1);
+
+
+        }
+ 
+    }
+}
